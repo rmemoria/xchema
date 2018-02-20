2601f8706a02412d29aff8d4c54ca7222d858f55
Ricardo <ricalima@amazon.com>
2018-02-19 16:17:12 -0800

    Custom validator tests

--- test/custom-validators-test.js
+++ test/custom-validators-test.js
@@ -77,7 +77,74 @@ describe('Custom validators', () => {
             });
     });
 
-    it('Schema level validation', () => {
-        // TO BE DONE
+    it('Custom validator in schema', () => {
+        const errmsg = 'Name cannot be vancouver';
+        const sc = {
+            properties: {
+                name: {
+                    type: 'string'
+                }
+            },
+            validator: {
+                isValid: doc => doc.name != 'vancouver',
+                message: errmsg,
+                code: 'INVALID_NAME'
+            }
+        };
+
+        return validator.validate({ name: 'vancouver' }, sc)
+            .catch(errs => {
+                assert.equal(errs.length, 1);
+                const err = errs[0];
+                assert.equal(err.property, null);
+                assert.equal(err.message, errmsg);
+                assert.equal(err.code, 'INVALID_NAME');
+            });
+    });
+
+    it('Skip custom validator in case of invalid property', () => {
+        const sc = {
+            properties: {
+                name: {
+                    type: 'string',
+                    required: true,
+                    validator: doc => doc.name != 'vancouver'
+                }
+            }
+        };
+
+        return validator.validate({ name: null }, sc)
+            .catch(errs => {
+                assert(errs);
+                assert.equal(errs.length, 1);
+                const err = errs[0];
+                assert.equal(err.property, 'name');
+                assert.equal(err.code, 'VALUE_REQUIRED');
+            });
+    });
+
+    it('Simple function validator', () => {
+        const errmsg = 'Cannot be Vancouver';
+        const sc = {
+            properties: {
+                name: {
+                    type: 'string',
+                    validator: doc => doc.name.toLowerCase() != 'vancouver' ?
+                        null : errmsg
+                }
+            }
+        };
+
+        return validator.validate({ name: 'Rio' }, sc)
+            .then(() => {
+                return validator.validate({ name: 'Vancouver' }, sc);
+            })
+            .catch(errs => {
+                assert(errs);
+                const err = errs[0];
+                assert.equal(err.property, 'name');
+                assert.equal(err.message, errmsg);
+                assert.equal(err.code, null);
+            });
     });
 });
